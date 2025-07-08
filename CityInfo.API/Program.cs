c4d56498507271c0a166ea9b89c6fd95044ea204
noboni <noboni5199@gmail.com>
2025-07-08 12:10:18 +0200

    XML type worked and get file using perfect type

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(options =>
 {
     options.ReturnHttpNotAcceptable = true;
-});
+}).AddXmlDataContractSerializerFormatters();
 
 builder.Services.AddProblemDetails(options =>
 {
@@ -17,6 +19,7 @@ builder.Services.AddProblemDetails(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
